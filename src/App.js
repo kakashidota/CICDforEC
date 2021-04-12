658db6f7450071162738210d1848d154c9542b04
Robin Kamo <robin.kamo@codic.se>
2021-04-12 13:54:43 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Häftiga saker händer på MAIN/PRODUKTION
+          Edit <code>src/App.js</code> and save to reload. Häftiga saker händer på MAIN/PRODUKTIONt
         </p>
         <a
           className="App-link"
