fcac54d8d95ab2775346c9b9e918893ab3087b43
Robin Kamo <robin.kamo@codic.se>
2021-04-12 14:27:33 +0200

    Changed the paragraph to staging

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Häftiga saker händer på MAIN/PRODUKTIONt
+          Edit <code>src/App.js</code> and save to reload. Häftiga saker händer på STAGING
         </p>
+          
         <a
           className="App-link"
           href="https://reactjs.org"
